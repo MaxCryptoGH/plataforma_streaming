9c65d5f2e7e7eba11e690618c12485051e09b40b
Massimiliano <cirinomaximiliano@gmail.com>
2022-09-08 18:33:49 +0200

    Actualizacion de tablas y botones

--- streapp/forms.py
+++ streapp/forms.py
@@ -13,20 +13,22 @@ genero= [
     ]
 
 # Clases películas, series y documentales -----------------
+ # name = forms.CharField(label='name', widget=forms.TextInput(attrs={'placeholder': 'Please enter your name'}))
+
 class PrimerFormulario(forms.Form):
     nombre = forms.CharField(label="Nombre de la película")
     genero = forms.CharField(label="Género", widget=forms.Select(choices=genero))
-    fecha = forms.DateField(label="Fecha(aaaa/mm/dd)")
+    fecha = forms.DateField(label="Fecha", widget=forms.TextInput(attrs={'placeholder': 'aaaa/mm/dd'}))
     aptoPara = forms.IntegerField(label="Edad requerida")
 
 class SeriesFormulario(forms.Form):
     nombre = forms.CharField(label="Nombre de la serie")
     genero = forms.CharField(label="Género", widget=forms.Select(choices=genero))
-    fecha = forms.DateField(label="Fecha(aaaa/mm/dd)")
+    fecha = forms.DateField(label="Fecha", widget=forms.TextInput(attrs={'placeholder': 'aaaa/mm/dd'}))
     aptoPara = forms.IntegerField(label="Edad requerida")
 
 class DocumentalesFormulario(forms.Form):
     nombre = forms.CharField(label="Nombre de la documental")
     genero = forms.CharField(label="Género", widget=forms.Select(choices=genero))
-    fecha = forms.DateField(label="Fecha(aaaa/mm/dd)")
+    fecha = forms.DateField(label="Fecha", widget=forms.TextInput(attrs={'placeholder': 'aaaa/mm/dd'}))
     aptoPara = forms.IntegerField(label="Edad requerida")
